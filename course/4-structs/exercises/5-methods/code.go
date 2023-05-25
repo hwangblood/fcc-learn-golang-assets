cb5ceb521471c6cc3aeb434e8b106c451af0976b
hwangblood <hwangblood@gmail.com>
2023-05-25 10:42:32 +0800

    Ch 4. Structs

--- course/4-structs/exercises/5-methods/code.go
+++ course/4-structs/exercises/5-methods/code.go
@@ -7,6 +7,10 @@ type authenticationInfo struct {
 	password string
 }
 
+func (authInfo authenticationInfo) getBasicAuth() string {
+	return fmt.Sprintf("Authorization: Basic %s:%s", authInfo.username, authInfo.password)
+}
+
 // ?
 
 // don't touch below this line
